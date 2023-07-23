093ccc8a692f0a1e146b4e92c6a1ff4fa47d9660
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2023-07-22 20:25:30 -0400

    chore(prettier): prettier all code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,35 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        ecmaFeatures: {
-            jsx: true,
-        },
-    },
-    "plugins": [
-        "react",
-        "react-hooks",
-        "@typescript-eslint"
-    ],
-    rules: {
-        // These rules are enabled by default, but we don't want
-        "some-annoying-rule": "off", // (conflicts with XYZ preference)
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:react-hooks/recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:perfectionist/recommended-natural",
+	],
+	overrides: [],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	plugins: ["react", "react-hooks", "@typescript-eslint", "perfectionist"],
+	rules: {
+		// These rules are enabled by default, but we don't want
+		"some-annoying-rule": "off", // (conflicts with XYZ preference)
 
-        // Todo: these rules might be useful; we should investigate each
-        "powerful-rule": "off", // (#123)
-    },
-    settings: {
-        react: {
-            version: "detect",
-        },
-    },
-}
+		// Todo: these rules might be useful; we should investigate each
+		"powerful-rule": "off", // (#123)
+	},
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
+};

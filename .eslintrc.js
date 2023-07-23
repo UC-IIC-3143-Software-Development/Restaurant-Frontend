0d2312e933db3258df3dbd51be12c565df9ae5c0
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2023-07-23 11:45:06 -0400

    chore(prettier): apply prettier tabs false by default

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,42 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: [
-		"eslint:recommended",
-		"plugin:react/recommended",
-		"plugin:react-hooks/recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:perfectionist/recommended-natural",
-	],
-	overrides: [],
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		ecmaVersion: "latest",
-		sourceType: "module",
-		ecmaFeatures: {
-			jsx: true,
-		},
-	},
-	plugins: ["react", "react-hooks", "@typescript-eslint", "perfectionist"],
-	ignorePatterns: [
-		".eslintrc.js",
-		"**/react-app-env.d.ts",
-		"**/reportWebVitals.ts",
-		"setupTests.ts",
-	],
-	rules: {
-		// These rules are enabled by default, but we don't want
-		"some-annoying-rule": "off", // (conflicts with XYZ preference)
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:perfectionist/recommended-natural",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["react", "react-hooks", "@typescript-eslint", "perfectionist"],
+  ignorePatterns: [
+    ".eslintrc.js",
+    "**/react-app-env.d.ts",
+    "**/reportWebVitals.ts",
+    "setupTests.ts",
+    "cypress/**"
+  ],
+  rules: {
+    // These rules are enabled by default, but we don't want
+    "some-annoying-rule": "off", // (conflicts with XYZ preference)
 
-		// Todo: these rules might be useful; we should investigate each
-		"powerful-rule": "off", // (#123)
-	},
-	settings: {
-		react: {
-			version: "detect",
-		},
-	},
+    // Todo: these rules might be useful; we should investigate each
+    "powerful-rule": "off", // (#123)
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
 };

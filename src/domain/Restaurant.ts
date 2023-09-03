1859f5682f1ea654671b88c17d76b7cea62010d4
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2023-09-02 23:50:20 -0400

    feat(domain): add restaurant schema with zod validations

---
+++ src/domain/Restaurant.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+const RestaurantSchema = z.object({
+  address: z
+    .string()
+    .min(5, "Address must be at least 5 characters")
+    .max(100, "Address must be at most 100 characters"),
+  name: z
+    .string()
+    .min(1, "Name must be at least 1 character")
+    .max(50, "Name must be at most 50 characters"),
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type Restaurant = z.infer<typeof RestaurantSchema>;

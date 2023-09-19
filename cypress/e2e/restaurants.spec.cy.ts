4cf877be62052d412e59f6e5844c4b45b9126e36
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2023-09-18 22:11:33 -0300

    chore(e2e): add cypress intercept code for resturant api with the sucessful case test

--- cypress/e2e/restaurants.spec.cy.ts
+++ cypress/e2e/restaurants.spec.cy.ts
@@ -1,12 +1,40 @@
 describe("Restaurant application", function () {
-  it("Visits the restaurants page list", function () {
-    cy.visit("http://localhost:3000/");
-    cy.get('h2[data-test="heading"]').contains("Restaurants");
-  });
+    beforeEach(() => {
+        // Intercept the successful call to the restaurants API
+        cy.intercept('GET', '/restaurants', {
+            statusCode: 200,
+            body: [
+                {
+                    id: "1",
+                    name: 'Restaurant 1',
+                    address: "myAddress 1",
+                    contactEmail: "myRestaurant1@myResturant.com"
+                },
+                {
+                    id: "2",
+                    name: 'Restaurant 2',
+                    address: "myAddress 2",
+                    contactEmail: "myRestaurant2@myResturant.com"
+                },
+            ]
+        }).as('getRestaurants');
+    });
+
+    it("Render the Restaurant header", function () {
+        cy.visit("/");
+        cy.get('h2[data-test="heading"]').contains("Restaurants");
+    });
+
+    it("Render the restaurant list fetched from the backend api", () => {
+        cy.visit("/");
+
+        // Wait for the intercepted API call to finish before proceeding
+        cy.wait('@getRestaurants');
+
+        cy.get('div[data-test="restaurant-list"]').should("exist");
+        cy.get("div.restaurant-item").should("have.length", 2);
+    });
+
 });
 
-it("Shows a book list", () => {
-  cy.visit("http://localhost:3000/");
-  cy.get('div[data-test="restaurant-list"]').should("exist");
-  cy.get("div.restaurant-item").should("have.length", 2);
-});
+

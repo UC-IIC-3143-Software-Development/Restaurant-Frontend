d8db046b42d6b817f94f43e7406b30f6d11a06ac
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2023-09-19 20:51:58 -0300

    feat(restaurant-detail): add case when props object doesn't contain a description field

--- src/components/RestaurantDetail/RestaurantDetail.test.tsx
+++ src/components/RestaurantDetail/RestaurantDetail.test.tsx
@@ -18,4 +18,18 @@ describe("RestaurantDetail", () => {
     const title = screen.getByRole("heading");
     expect(title.innerHTML).toEqual(props.restaurant.name);
   });
+
+  it("displays the restaurant name when no description was given", () => {
+    const props = {
+      restaurant: {
+        address: "Some address",
+        contactEmail: "myContactEmail@contactEmail.com",
+        id: "0",
+        name: "Restaurant1",
+      },
+    };
+    render(<RestaurantDetail {...props} />);
+    const description = screen.getByTestId("restaurant-description");
+    expect(description).toHaveTextContent(props.restaurant.name);
+  });
 });
--- src/components/RestaurantDetail/RestaurantDetail.tsx
+++ src/components/RestaurantDetail/RestaurantDetail.tsx
@@ -2,12 +2,20 @@ import React from "react";
 
 import { Restaurant } from "../../domain/Restaurant";
 
+const getDescriptionFor = (restaurant: Restaurant) => {
+  return restaurant.description ? restaurant.description : restaurant.name;
+};
+
 const RestaurantDetail = ({ restaurant }: { restaurant: Restaurant }) => {
   return (
     <div className="detail">
-      {" "}
       <h2 className="restaurant-title">{restaurant.name}</h2>{" "}
-      <p className="restaurant-description">{restaurant.description}</p>
+      <p
+        className="restaurant-description"
+        data-testid="restaurant-description"
+      >
+        {getDescriptionFor(restaurant)}
+      </p>
     </div>
   );
 };

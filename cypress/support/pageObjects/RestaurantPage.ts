1ed69ce70ca6c25866008186e0e313735a9699d4
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2023-11-12 21:32:05 -0300

    chore(e2e): refactor e2e test into pageObject pattern fix perfectionist plugin

--- cypress/support/pageObjects/RestaurantPage.ts
+++ cypress/support/pageObjects/RestaurantPage.ts
@@ -1,50 +1,50 @@
 class RestaurantPage {
-  visit() {
-    cy.visit("/");
+  clickOnFirstRestaurantDetails() {
+    this.getRestaurantItems().contains("View Details").eq(0).click();
+  }
+
+  currentUrl() {
+    return cy.url();
   }
 
   getRestaurantHeading() {
     return cy.get('h2[data-test="heading"]');
   }
 
-  getRestaurantList() {
-    return cy.get('div[data-test="restaurant-list"]');
-  }
-
   getRestaurantItems() {
     return cy.get("div.restaurant-item");
   }
 
-  clickOnFirstRestaurantDetails() {
-    this.getRestaurantItems().contains("View Details").eq(0).click();
+  getRestaurantList() {
+    return cy.get('div[data-test="restaurant-list"]');
   }
 
   interceptRestaurants() {
     cy.intercept("GET", "/restaurants", {
-      statusCode: 200,
       body: [
         {
-          id: "1",
-          name: "Repollo",
           address: "myAddress 1",
           contactEmail: "repollo@myResturant.com",
+          id: "1",
+          name: "Repollo",
         },
         {
-          id: "2",
-          name: "El Pollo del 8",
           address: "myAddress 2",
           contactEmail: "pollodel8@myResturant.com",
+          id: "2",
+          name: "El Pollo del 8",
         },
       ],
+      statusCode: 200,
     }).as("getRestaurants");
+  }
+
+  visit() {
+    cy.visit("/");
   }
 
   waitForRestaurants() {
     cy.wait("@getRestaurants");
-  }
-
-  currentUrl() {
-    return cy.url();
   }
 }
 
